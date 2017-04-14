93e20a2e2b01032392766a4826ec533a515cd468
Alex Wang <aw1621107@gmail.com>
2017-04-14 19:30:35 -0400

    m4: Move keg_only above depends_on
    Fix issue from brew audit --strict --online

--- Formula/m4.rb
+++ Formula/m4.rb
@@ -13,10 +13,10 @@ class M4 < Formula
     sha256 "00d9327f2e8a59996228569bf4faff1c6550653eb3e20353e77f73a34063f3eb" => :yosemite
   end
 
+  keg_only :provided_by_osx
+
   depends_on "gettext" => :build
   depends_on "libsigsegv"
-
-  keg_only :provided_by_osx
 
   # Fix crash from usage of %n in dynamic format strings on High Sierra
   # Patch credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
